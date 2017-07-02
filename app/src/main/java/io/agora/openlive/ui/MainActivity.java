aad26686bcdfe6a02aa2876ba541b5fa08c8148e
Siba Siddique <sibasid@gmail.com>
2017-07-02 11:32:16 +0400

    Project Push

---
+++ app/src/main/java/io/agora/openlive/ui/MainActivity.java
@@ -0,0 +1,224 @@
+package io.agora.openlive.ui;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import dji.sdk.base.BaseProduct;
+import dji.sdk.products.Aircraft;
+import io.agora.openlive.AGApplication;
+import io.agora.openlive.R;
+import io.agora.openlive.model.ConstantApp;
+import io.agora.rtc.Constants;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends BaseActivity implements View.OnClickListener{
+    private static final String TAG = MainActivity.class.getName();
+
+    private TextView mTextConnectionStatus;
+    private TextView mTextProduct;
+    private TextView mTextModelAvailable;
+    private Button mBtnOpen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initUIandEvent() {
+        EditText textRoomName = (EditText) findViewById(R.id.room_name);
+
+        mTextConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
+        mTextModelAvailable = (TextView) findViewById(R.id.text_model_available);
+        mTextProduct = (TextView) findViewById(R.id.text_product_info);
+        mBtnOpen = (Button) findViewById(R.id.btn_open);
+        mBtnOpen.setOnClickListener(this);
+        mBtnOpen.setEnabled(false);
+
+        textRoomName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean isEmpty = s.toString().isEmpty();
+                findViewById(R.id.button_join).setEnabled(!isEmpty);
+            }
+        });
+    }
+
+    @Override
+    protected void deInitUIandEvent() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                forwardToSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onClickJoin(View view) {
+        // show dialog to choose role
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.msg_choose_role);
+        builder.setNegativeButton(R.string.label_audience, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.this.forwardToLiveRoom(Constants.CLIENT_ROLE_AUDIENCE);
+            }
+        });
+        builder.setPositiveButton(R.string.label_broadcaster, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MainActivity.this.forwardToLiveRoom(Constants.CLIENT_ROLE_BROADCASTER);
+            }
+        });
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+    }
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            refreshSDKRelativeUI();
+        }
+    };
+
+    private void updateTitleBar() {
+        boolean ret = false;
+        BaseProduct product = AGApplication.getProductInstance();
+        if (product != null) {
+            if(product.isConnected()) {
+                //The product is connected
+                showToast(AGApplication.getProductInstance().getModel() + " Connected");
+                ret = true;
+            } else {
+                if(product instanceof Aircraft) {
+                    Aircraft aircraft = (Aircraft)product;
+                    if(aircraft.getRemoteController() != null && aircraft.getRemoteController().isConnected()) {
+                        // The product is not connected, but the remote controller is connected
+                        showToast("only RC Connected");
+                        ret = true;
+                    }
+                }
+            }
+        }
+
+        if(!ret) {
+            // The product or the remote controller are not connected.
+            showToast("Disconnected");
+        }
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateVersion() {
+        String version = null;
+        if(AGApplication.getProductInstance() != null) {
+            version = AGApplication.getProductInstance().getFirmwarePackageVersion();
+        }
+
+        if(version == null) {
+            mTextModelAvailable.setText("N/A"); //Firmware version:
+        } else {
+            mTextModelAvailable.setText(version); //"Firmware version: " +
+        }
+    }
+    private void refreshSDKRelativeUI() {
+
+        BaseProduct mProduct = AGApplication.getProductInstance();
+        Log.v(TAG, "refreshSDKRelativeUI");
+
+        if (null != mProduct && mProduct.isConnected()) {
+            Log.v(TAG, "refreshSDK: True");
+            mBtnOpen.setEnabled(true);
+
+            String str = mProduct instanceof Aircraft ? "DJIAircraft" : "DJIHandHeld";
+            mTextConnectionStatus.setText("Status: " + str + " connected");
+            updateVersion();
+
+            if (null != mProduct.getModel()) {
+                mTextProduct.setText("" + mProduct.getModel().getDisplayName());
+            } else {
+                mTextProduct.setText(R.string.product_information);
+            }
+        } else {
+            Log.v(TAG, "refreshSDK: False");
+            mBtnOpen.setEnabled(false);
+
+            mTextProduct.setText(R.string.product_information);
+            mTextConnectionStatus.setText(R.string.connection_loose);
+        }
+    }
+
+    public void forwardToLiveRoom(int cRole) {
+        final EditText v_room = (EditText) findViewById(R.id.room_name);
+        String room = v_room.getText().toString();
+
+        Intent i = new Intent(MainActivity.this, LiveRoomActivity.class);
+        i.putExtra(ConstantApp.ACTION_KEY_CROLE, cRole);
+        i.putExtra(ConstantApp.ACTION_KEY_ROOM_NAME, room);
+
+        startActivity(i);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.btn_open: {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    public void forwardToSettings() {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
+    }
+}
